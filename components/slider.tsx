d19ca341d1ab5ab98694b7dbe99a091eafd80592
MekkaLabs <labsmekka@gmail.com>
2022-04-24 11:48:20 -0300

    Update slider.tsx

--- components/slider.tsx
+++ components/slider.tsx
@@ -6,6 +6,7 @@ interface Props {
     initialOffsetX: number
     className: string
     contentWidth: number
+    children: ReactNode
 }
 
 const SliderContainer: React.FC<Props> = ({ children, initialOffsetX, className, contentWidth }) => {
